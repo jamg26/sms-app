ee15ec30d388ea7d0e184ceafcd6ca63085a90b9
Jamuel Galicia <jammmg26@gmail.com>
2019-02-21 03:56:57 +0800

    Add project files.

---
+++ sms-jamgph/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace sms_jamgph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.txtApi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtApi);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtMsg);
+            this.groupBox1.Controls.Add(this.txtNo);
+            this.groupBox1.Controls.Add(this.sendBtn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(362, 173);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(164, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 7);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MESSAGE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 7);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "MOBILE NUMBER";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(6, 55);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(350, 70);
+            this.txtMsg.TabIndex = 2;
+            this.txtMsg.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(87, 19);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(193, 20);
+            this.txtNo.TabIndex = 1;
+            this.txtNo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(258, 144);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 0;
+            this.sendBtn.Text = "SEND";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // txtApi
+            // 
+            this.txtApi.Location = new System.Drawing.Point(7, 144);
+            this.txtApi.Name = "txtApi";
+            this.txtApi.Size = new System.Drawing.Size(100, 20);
+            this.txtApi.TabIndex = 5;
+            this.txtApi.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 7);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "API CODE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 197);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SMS-JAMGPH.COM";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtApi;
+    }
+}
+
---
+++ sms-jamgph/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sms_jamgph
+{
+    public partial class Form1 : Form
+    {
+        public static string no, msg, api;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            no = txtNo.Text;
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            sendBtn.Hide();
+            string[] err = {"Message Sent!", "Invalid Number", "Number not supported", "Invalid API Code", "Only 10 sms per day", "Too many message characters", "System Offline", "Expired API Code", "Error", "Cannot be empty", "Recipients mobile has been blocked", "Invalid Request", "Invalid sender ID", "Invalid Server Number"};
+            dynamic result = itexmo(no, msg, api);
+            int res = Convert.ToInt32(result);
+            MessageBox.Show(err[res]);
+            sendBtn.Show();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            msg = txtMsg.Text + "\n\n\n\n\n\n\nhttps://jamgph.com";
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            api = txtApi.Text;
+        }
+
+        public object itexmo(string Number, string Message, string API_CODE)
+        {
+            object functionReturnValue = null;
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                System.Collections.Specialized.NameValueCollection parameter = new System.Collections.Specialized.NameValueCollection();
+                string url = "https://www.itexmo.com/php_api/api.php";
+                parameter.Add("1", Number);
+                parameter.Add("2", Message);
+                parameter.Add("3", API_CODE);
+                dynamic rpb = client.UploadValues(url, "POST", parameter);
+                functionReturnValue = (new System.Text.UTF8Encoding()).GetString(rpb);
+            }
+            return functionReturnValue;
+        }
+
+    }
+}
